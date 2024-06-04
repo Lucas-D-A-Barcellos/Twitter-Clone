291511e6235bbf81a4bc5e185b11ca21b911070a
Lucas-D-A-Barcellos <127368679+Lucas-D-A-Barcellos@users.noreply.github.com>
2024-06-04 17:50:23 -0300

    Add files via upload

---
+++ App/Controllers/AppController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Controllers;
+
+//ESTRUTURA FRAMEWORK
+use MF\Controller\Action;
+use MF\Model\Container;
+
+class AppController extends Action {
+
+    public function timeline(){
+
+        $this->validaAtenticacao();
+
+        //recuperação dos tweets
+        $tweet = Container::getModel('tweet');
+
+        $tweet->__set('id_usuario', $_SESSION['id']);
+
+        $tweets = $tweet->getAll();
+
+        $this->view->tweets = $tweets;
+
+
+        $usuario = Container::getModel('Usuario');
+        $usuario->__set('id', $_SESSION['id']);
+
+        $this->view->info_usuario  = $usuario->getInfoUsuario();
+        $this->view->total_tweets  = $usuario->getTotalTweets();
+        $this->view->total_seguindo  = $usuario->getTotalSeguindo();
+        $this->view->total_seguidores  = $usuario->getTotalSeguidores();
+
+        $this->render('timeline');           
+       
+    }
+
+    public function tweet(){
+
+        $this->validaAtenticacao();
+
+        $tweet = Container::getModel('Tweet');
+
+        $tweet->__set('tweet', $_POST['tweet']);
+        $tweet->__set('id_usuario', $_SESSION['id']);
+        
+        $tweet->salvar();
+
+        header('Location: /timeline');           
+        
+    }
+
+    public function validaAtenticacao(){
+
+        session_start();
+
+        if (!isset($_SESSION['id']) || $_SESSION['id'] == '' || $_SESSION['nome'] == '' || !isset($_SESSION['nome'])) {
+
+            header('Location: /?login=erro');
+        }
+    }
+
+    public function quemSeguir(){
+
+        $this->validaAtenticacao();
+
+        $pesquisarPor = isset($_GET['pesquisarPor']) ? $_GET['pesquisarPor'] : '';
+        
+        $usuarios = array();
+
+        if ($pesquisarPor != '') {
+            
+            $usuario = Container::getModel('Usuario');
+            $usuario->__set('nome', $pesquisarPor);
+            $usuario->__set('id', $_SESSION['id']);
+            $usuarios = $usuario->getAll();
+        }
+
+        $this->view->usuarios = $usuarios;
+
+        $usuario = Container::getModel('Usuario');
+        $usuario->__set('id', $_SESSION['id']);
+
+        $this->view->info_usuario  = $usuario->getInfoUsuario();
+        $this->view->total_tweets  = $usuario->getTotalTweets();
+        $this->view->total_seguindo  = $usuario->getTotalSeguindo();
+        $this->view->total_seguidores  = $usuario->getTotalSeguidores();
+
+        $this->render('quemSeguir');
+
+    }
+
+    public function acao(){
+
+        $this->validaAtenticacao();
+
+        $acao = isset($_GET['acao']) ? $_GET['acao'] : '';
+        $id_usuario_seguindo = isset($_GET['id_usuario']) ? $_GET['id_usuario'] : '';
+
+        $usuario = Container::getModel('Usuario');
+        $usuario->__set('id', $_SESSION['id']);
+
+        if ($acao == 'seguir') {
+
+            $usuario->seguirUsuario($id_usuario_seguindo);
+
+        }elseif ($acao == 'deixar_de_seguir') {
+
+            $usuario->deixarSeguirUsuario($id_usuario_seguindo);
+
+        }
+
+        header('Location: /quem_seguir');
+
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+    //init_set('eror_reporting'. 'E_STRICT');
+
+    require_once "../vendor/autoload.php";
+
+    $route = new \App\Route;
+
+?>
